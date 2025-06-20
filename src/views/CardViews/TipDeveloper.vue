f9f09d2f504e95ba6c93542bb7236bfae2332c11
Michael Totaro <mstotaro@gmail.com>
2025-06-20 11:04:23 -0400

    Payment pictures

--- src/views/CardViews/TipDeveloper.vue
+++ src/views/CardViews/TipDeveloper.vue
@@ -1,12 +1,21 @@
 <template>
-  <p>Tip the DEVELOPER</p>
+  <div>
+    <p>Tip the DEVELOPER</p>
+    <img :src="PayPalPicture" alt="PayPal" />
+    <img :src="VenmoPicture" alt="Venmo" />
+    <img :src="StripePicture" alt="Stripe" />
+  </div>
 </template>
 
 <script setup>
-
+import PayPalPicture from '../../assets/payments/paypal.jpg';
+import VenmoPicture from '../../assets/payments/venmo.jpg';
+import StripePicture from '../../assets/payments/stripe.jpg';
 </script>
 
-
 <style scoped>
-
+img {
+  width: 200px;
+  margin: 10px;
+}
 </style>
